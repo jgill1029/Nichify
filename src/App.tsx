1f2698c364b67e9fe778d90602e695e8b9542bc3
jgill1029 <jgill1029@gmail.com>
2023-09-18 21:17:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>Nichify</Router>
+    </>
+  );
+}
+
+export default App;
